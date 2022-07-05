bd19867210cb9f4a96c4df9ef462a1e18dd2fe37
noor-s-hassan <108189131+noor-s-hassan@users.noreply.github.com>
2022-07-05 05:23:54 -0400

    change

--- app/sendMail.py
+++ app/sendMail.py
@@ -2,8 +2,8 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 
-subject_pref = 'BlogVolution Membership!'
-sender_email = 'splashpitch@gmail.com'
+subject_pref = 'Blog Membership!'
+sender_email = 'n.bot@gmail.com'
 
 def mail_message(subject,template,to,**kwargs):
    
